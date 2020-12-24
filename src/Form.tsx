bed688760bdd9c447db413bdfabb2924eb3741a1
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-23 20:04:24 -0700

    Add on change function

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,15 +1,18 @@
 import React, {useState,useEffect} from 'react';
 function Form (){
-  let [name:string, changeState] = useState('')
-
+  let [toDo, changeState] = useState('')
+function createToDo(){
+  changeState(toDo)
+}
   return(
       <section>
-        <form onSubmit={submitForm}>
+        <form >
           <input
             value={name}
             placeholder="Add A Todo"
-            onChange={e => createToDo(e.target.value)} />
-          <button>Add Todo</button>
+            value = {toDo} 
+            onChange={e => createToDo(e.target.value)}/>
+          <button onSubmit = {e =>{addToDo(toDo)}} >Add Todo</button>
         </form>
       </section>
   )
